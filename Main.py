2886bddd1d1df411bedefe44ab569ca76ee8ad8e
CarlosJNF <136031992+CarlosJNF@users.noreply.github.com>
2023-06-16 01:32:36 +0000

    cambios 1, clase 2

---
+++ Main.py
@@ -0,0 +1,10 @@
+from matematicas import sumar, restar, multiplicar, dividir
+
+print("Suma: ", sumar(2,3))
+print("Resta:", restar(19,5))
+print("Multiplicacion: ", multiplicar(2,5))
+print("Division: ", dividir(8,3))
+
+from geometria import distancia
+print("Distancia:", distancia(8,4,2,10))
+
